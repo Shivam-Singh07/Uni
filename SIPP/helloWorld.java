b31fd9f4eab25338d10be791c887d751018ddb56
Shivam Singh <s9229023@gmail.com>
2025-06-09 09:19:12 +0530

    first Prime

--- SIPP/helloWorld.java
+++ SIPP/helloWorld.java
@@ -1,6 +1,27 @@
+import java.util.*;
 public class helloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == 1)
+        {
+            System.out.println("Not Prime");
+        }
+        for(int i = 2;i<n/2;i++)
+        {
+            if(n % i == 0)
+            {
+                System.out.println("Not Prime");
+                break;
+            }
+            else
+            {
+                System.out.println("Prime");
+                break;
+            }
+        }
     }
 }
